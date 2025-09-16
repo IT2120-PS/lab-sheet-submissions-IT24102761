9a3304e9b25c4d6a5a46767351cb66143e56b9b5
Edirisooriya Arachchige R.P.E <it24102761@my.sliit.lk>
2025-09-16 23:12:08 +0530

    Lab_07

---
+++ Lab Sheet 07/IT24102761_Lab07.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\Asus\\OneDrive - Sri Lanka Institute of Information Technology\\Documents\\SLIIT LAB\\Y2S1\\PS\\Lab 07")
+
+##Question 1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+##Question 2
+pexp(2, rate = 1/3)
+
+##Question 3
+##Part 1
+1 - pnorm(130, mean = 100, sd = 15)
+
+##Part 2
+qnorm(0.95, mean = 100, sd = 15)
