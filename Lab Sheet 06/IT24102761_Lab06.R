566e490a0c02fbf02a4628e2bb2f1abd91d1709c
Edirisooriya Arachchige R.P.E <it24102761@my.sliit.lk>
2025-09-16 22:40:04 +0530

    Lab_06

---
+++ Lab Sheet 06/IT24102761_Lab06.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\Asus\\OneDrive - Sri Lanka Institute of Information Technology\\Documents\\SLIIT LAB\\Y2S1\\PS\\Lab 06")
+
+
+#1
+#i - Binomial Distribution
+  
+#ii - 
+1 - pbinom(46,50,0.85, lower.tail = TRUE)
+
+#2
+
+#i. - Number of  customers call per hour
+
+#ii - Possion Distribution
+
+#iii
+dpois(15,12)
