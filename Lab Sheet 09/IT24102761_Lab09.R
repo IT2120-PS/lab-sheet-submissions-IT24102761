9fdb2a54bbd07719c9fc4d563e876d272eefbc45
Edirisooriya Arachchige R.P.E <it24102761@my.sliit.lk>
2025-10-10 14:14:36 +0530

    Lab_09

---
+++ Lab Sheet 09/IT24102761_Lab09.R
@@ -0,0 +1,11 @@
+getwd()
+setwd("C:\\Users\\Asus\\OneDrive - Sri Lanka Institute of Information Technology\\Desktop\\IT24102761")
+
+#Exercise
+#1
+#i
+y <- rnorm(25, mean=45, sd = 2)
+
+##ii
+t.test(y, mu=46, alternative= "less")
+
